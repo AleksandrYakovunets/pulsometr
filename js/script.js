49870774df1bc8e2d6448f7cd70f019092d4a619
AleksandrYakovunets <deltapilot.mk@gmail.com>
2023-09-13 13:41:05 +0200

    change right_arrow.png

--- js/script.js
+++ js/script.js
@@ -8,6 +8,6 @@ $(document).ready(function () {
     cssEase: 'linear',
     //adaptiveHeight: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="../icons/carousel/left_arrow.png"/></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="../icons/carousel/right arrow.png"/></button>'
+    nextArrow: '<button type="button" class="slick-next"><img src="../icons/carousel/right_arrow.png"/></button>'
   });
 });
