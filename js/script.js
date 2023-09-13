285256ebb65a6e7b31ee6c10b7565d823f4f5ad6
AleksandrYakovunets <deltapilot.mk@gmail.com>
2023-09-13 13:35:07 +0200

    add section carousel

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $(".carousel_inner").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1200,
+    fade: true,
+    cssEase: 'linear',
+    //adaptiveHeight: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="../icons/carousel/left_arrow.png"/></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../icons/carousel/right arrow.png"/></button>'
+  });
+});
