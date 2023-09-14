090f119957d4a9fa90746bc2130e65229884d7f3
AleksandrYakovunets <deltapilot.mk@gmail.com>
2023-09-14 10:59:59 +0200

    fixing paths

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     fade: true,
     cssEase: "linear",
     //adaptiveHeight: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="../icons/carousel/left_arrow.png"/></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="../icons/carousel/right_arrow.png"/></button>'
+    prevArrow: '<button type="button" class="slick-prev"><img src="/icons/carousel/left_arrow.png"/></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="/icons/carousel/right_arrow.png"/></button>'
   });
 });
