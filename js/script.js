75e3a267c46f9d971c7157f624d76dde3a97ad97
AleksandrYakovunets <deltapilot.mk@gmail.com>
2023-09-15 13:00:53 +0200

    slider adaptation

--- js/script.js
+++ js/script.js
@@ -8,6 +8,14 @@ $(document).ready(function () {
     cssEase: "linear",
     //adaptiveHeight: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="icons/carousel/left_arrow.png"/></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="icons/carousel/right_arrow.png"/></button>'
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/carousel/right_arrow.png"/></button>',
+    responsive: [{
+      breakpoint: 991,
+      settings: {
+        arrows: false,
+        dots: true,
+        adaptiveHeight: true
+      }
+    }]
   });
 });
