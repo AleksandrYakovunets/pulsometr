0d415d00d585d5f661e88d98eac017c7d91a91bc
AleksandrYakovunets <deltapilot.mk@gmail.com>
2023-09-27 12:59:41 +0200

    add modal window consultation and order

--- js/script.js
+++ js/script.js
@@ -7,38 +7,59 @@ $(document).ready(function () {
     fade: true,
     cssEase: "linear",
     //adaptiveHeight: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="icons/carousel/left_arrow.png"/></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="icons/carousel/right_arrow.png"/></button>',
-    responsive: [{
-      breakpoint: 991,
-      settings: {
-        arrows: false,
-        dots: true,
-        adaptiveHeight: true
-      }
-    }]
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="icons/carousel/left_arrow.png"/></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="icons/carousel/right_arrow.png"/></button>',
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          arrows: false,
+          dots: true,
+          adaptiveHeight: true,
+        },
+      },
+    ],
   });
 
-  $('ul.catalog_tabs').on('click', 'li:not(.catalog_active)', function() {
-    
+  $("ul.catalog_tabs").on("click", "li:not(.catalog_active)", function () {
     $(this)
-      .addClass('catalog_active').siblings().removeClass('catalog_active')
-      .closest('div.container').find('div.catalog_content').removeClass('catalog_content_active').eq($(this).index()).addClass('catalog_content_active');
+      .addClass("catalog_active")
+      .siblings()
+      .removeClass("catalog_active")
+      .closest("div.container")
+      .find("div.catalog_content")
+      .removeClass("catalog_content_active")
+      .eq($(this).index())
+      .addClass("catalog_content_active");
   });
 
-  $('.catalog_item_link').each(function(i){
-    $(this).on('click', function(e){
+  $(".catalog_item_link").each(function (i) {
+    $(this).on("click", function (e) {
       e.preventDefault();
-      $('.catalog_item_detail').eq(i).toggleClass('catalog_item_detail_active');
-      if($('.catalog_item_link').eq(i).html() == 'подробнее'){
-        $('.catalog_item_link').eq(i).html('назад');
-      } 
-      else {
-        $('.catalog_item_link').eq(i).html('подробнее');
+      $(".catalog_item_detail").eq(i).toggleClass("catalog_item_detail_active");
+      if ($(".catalog_item_link").eq(i).html() == "подробнее") {
+        $(".catalog_item_link").eq(i).html("назад");
+      } else {
+        $(".catalog_item_link").eq(i).html("подробнее");
       }
-    })
+    });
   });
 
+  //Modal
+  $("[data-modal = consultation]").on("click", function () {
+    $(".overlay, #consultation").fadeIn("slow");
+  });
+
+  $(".modal_close").on("click", function () {
+    $(".overlay, #consultation, #order, #thanks").fadeOut("slow");
+  });
+
+  $(".catalog_item_btn").each(function(i) {
+    $(this).on('click', function(){
+      $('#order .modal_descr').text($('.catalo_item_title').eq(i).text());
+      $(".overlay, #order").fadeIn("slow");
+    });
+  });
 });
-
-
